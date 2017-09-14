d463f8252d8ef643fcfe915db5b6795662796fe2
StapleButter <thetotalworm@gmail.com>
2017-09-14 02:30:11 +0200

    * build settings for Release * fix off-by-one error in bitmap drawing

--- src/libui_sdl/libui/windows/draw.cpp
+++ src/libui_sdl/libui/windows/draw.cpp
@@ -550,8 +550,8 @@ void uiDrawBitmapUpdate(uiDrawBitmap* bmp, const void* data)
 
 void uiDrawBitmapDraw(uiDrawContext* c, uiDrawBitmap* bmp, uiRect* srcrect, uiRect* dstrect)
 {
-    D2D_RECT_F _srcrect = D2D1::RectF(srcrect->X, srcrect->Y, srcrect->X+srcrect->Width-1, srcrect->Y+srcrect->Height-1);
-    D2D_RECT_F _dstrect = D2D1::RectF(dstrect->X, dstrect->Y, dstrect->X+dstrect->Width-1, dstrect->Y+dstrect->Height-1);
+    D2D_RECT_F _srcrect = D2D1::RectF(srcrect->X, srcrect->Y, srcrect->X+srcrect->Width, srcrect->Y+srcrect->Height);
+    D2D_RECT_F _dstrect = D2D1::RectF(dstrect->X, dstrect->Y, dstrect->X+dstrect->Width, dstrect->Y+dstrect->Height);
 
     c->rt->DrawBitmap(bmp->bmp, &_dstrect, 1.0f, D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, &_srcrect);
 }
