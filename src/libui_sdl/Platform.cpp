a837ad2b0d7b45368c4d6f1c2fc08ceced7baf9f
KrossX <krossx@yahoo.com>
2019-06-22 17:31:28 -0300

    Prevent NTDDI WINNT version conflict.

--- src/libui_sdl/Platform.cpp
+++ src/libui_sdl/Platform.cpp
@@ -28,7 +28,8 @@
 #include <string>
 
 #ifdef __WIN32__
-    #define NTDDI_VERSION		0x06000000 // GROSS FUCKING HACK
+    #define NTDDI_VERSION		NTDDI_WIN6 // GROSS FUCKING HACK
+    #define _WIN32_WINNT _WIN32_WINNT_WIN6
     #include <windows.h>
     //#include <knownfolders.h> // FUCK THAT SHIT
     extern "C" const GUID DECLSPEC_SELECTANY FOLDERID_RoamingAppData = {0x3eb685db, 0x65f9, 0x4cf6, {0xa0, 0x3a, 0xe3, 0xef, 0x65, 0x72, 0x9f, 0x3d}};
