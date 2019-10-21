e117da235e939a47e4dd08fbded68abc3165271d
Arisotura <thetotalworm@gmail.com>
2019-10-21 23:14:34 +0200

    smarter CP15 PU region updates. disable some useless logging.
    fixes #528

--- src/ARM.h
+++ src/ARM.h
@@ -211,7 +211,8 @@ public:
     void UpdateDTCMSetting();
     void UpdateITCMSetting();
 
-    void UpdatePURegions();
+    void UpdatePURegion(u32 n);
+    void UpdatePURegions(bool update_all);
 
     u32 RandomLineIndex();
 
