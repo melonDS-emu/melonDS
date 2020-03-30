104b2a03aad74121358828a2c7c3a1a636f66a42
Arisotura <thetotalworm@gmail.com>
2020-03-30 11:04:50 +0200

    properly handle ROMs with encrypted secure area

--- src/NDSCart.h
+++ src/NDSCart.h
@@ -44,6 +44,7 @@ void Reset();
 
 void DoSavestate(Savestate* file);
 
+void DecryptSecureArea(u8* out);
 bool LoadROM(const char* path, const char* sram, bool direct);
 void RelocateSave(const char* path, bool write);
 
