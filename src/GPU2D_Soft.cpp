fbe691a673333ca44de1e18eceb8443c308adc69
RSDuck <RSDuck@users.noreply.github.com>
2021-03-16 20:57:23 +0100

    fix read from unassigned variable for large BGs

--- src/GPU2D_Soft.cpp
+++ src/GPU2D_Soft.cpp
@@ -1464,7 +1464,6 @@ void SoftRenderer::DrawBG_Large(u32 line) // BG is always BG2
 {
     u16 bgcnt = CurUnit->BGCnt[2];
 
-    u32 tilesetaddr, tilemapaddr;
     u16* pal;
 
     // large BG sizes:
@@ -1539,7 +1538,7 @@ void SoftRenderer::DrawBG_Large(u32 line) // BG is always BG2
 
             if (!(finalX & ofxmask) && !(finalY & ofymask))
             {
-                color = bgvram[(tilemapaddr + (((finalY & ymask) >> 8) << yshift) + ((finalX & xmask) >> 8)) & bgvrammask];
+                color = bgvram[((((finalY & ymask) >> 8) << yshift) + ((finalX & xmask) >> 8)) & bgvrammask];
 
                 if (color)
                     drawPixel(&BGOBJLine[i], pal[color], 0x01000000<<2);
