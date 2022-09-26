7c9e37b156de6307512436c0aba6714a5196eddd
Arisotura <thetotalworm@gmail.com>
2022-09-26 20:37:25 +0200

    remove some cruft

--- src/frontend/qt_sdl/main.cpp
+++ src/frontend/qt_sdl/main.cpp
@@ -457,47 +457,6 @@ void EmuThread::run()
 
     char melontitle[100];
 
-    //QImage testimg("test.jpg");
-    //QImage testimg_conv = testimg.convertToFormat(QImage::Format_RGB32);
-
-    #if 0
-    const QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
-    for (const QCameraInfo &cameraInfo : cameras)
-        printf("CAMERAFAZIL: %s\n", cameraInfo.deviceName().toStdString().c_str());
-    QCamera* camera = new QCamera(cameras[0]);
-    CameraFrameDumper* dumper = new CameraFrameDumper();
-    //QCameraViewfinder* derp = new QCameraViewfinder();
-    printf("PROAON\n");
-    //camera->setCaptureMode(QCamera::CaptureVideo);
-    //camera->setCaptureMode(QCamera::CaptureViewfinder);
-    printf("PROOT\n");
-    //camera->unload();
-    camera->setViewfinder(dumper);
-    //camera->setViewfinder(derp);
-    //camera->load();
-
-    // if (status != QCamera::LoadedStatus || m_camera->state() == QCamera::ActiveState) { return
-    printf("STATUS %d STATE %d\n", camera->status(), camera->state());
-
-printf("CHIASSE\n");
-    /*QCameraViewfinderSettings settings;
-    auto zorp = camera->supportedViewfinderResolutions(settings);
-    for (auto& res : zorp) printf("RESOLUTION: %d x %d\n", res.width(), res.height());
-    auto zarp = camera->supportedViewfinderPixelFormats(settings);
-    for (auto& pf : zarp) printf("PIXEL FORMAT: %d\n", pf);
-
-    settings.setResolution(640, 480);
-    //settings.setPixelFormat(QVideoFrame::Format_RGB32);
-    settings.setPixelFormat(QVideoFrame::Format_YUYV);
-    printf("PRALIPET\n");
-    camera->setViewfinderSettings(settings);*/
-printf("PROULON\n");
-    //dumper->start();
-    //QVideoSurfaceFormat blarf(QSize(640,480), QVideoFrame::Format_RGB32);
-    //dumper->start(blarf);
-    camera->start();
-    #endif
-
     while (EmuRunning != 0)
     {
         Input::Process();
@@ -743,9 +702,6 @@ printf("PROULON\n");
             SDL_Delay(75);
         }
     }
-
-    //camera->stop();
-    //delete camera;
 
     EmuStatus = 0;
 
