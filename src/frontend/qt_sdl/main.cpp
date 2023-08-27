b4756c59441d83469d875ab96d6636c33707c0da
xenticore <xenticore@icloud.com>
2023-08-27 07:37:42 -0400

    Update macOS icon (#1609)
    * Add SVG icon
    
    * Update macOS icon
    
    * Don't force change the application icon at runtime on macOS

--- src/frontend/qt_sdl/main.cpp
+++ src/frontend/qt_sdl/main.cpp
@@ -3199,7 +3199,9 @@ void emuStop()
 MelonApplication::MelonApplication(int& argc, char** argv)
     : QApplication(argc, argv)
 {
+#ifndef __APPLE__
     setWindowIcon(QIcon(":/melon-icon"));
+#endif
 }
 
 bool MelonApplication::event(QEvent *event)
