3fd674f2e4c95ea65c942c30519b819e320ff92f
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-11-20 16:12:56 -0500

    Make some functions in FrontendUtil take a NDS&

--- src/frontend/FrontendUtil.h
+++ src/frontend/FrontendUtil.h
@@ -24,6 +24,10 @@
 #include <string>
 #include <vector>
 
+namespace melonDS
+{
+class NDS;
+}
 namespace Frontend
 {
 using namespace melonDS;
@@ -105,14 +109,14 @@ int AudioOut_GetNumSamples(int outlen);
 void AudioOut_Resample(s16* inbuf, int inlen, s16* outbuf, int outlen, int volume);
 
 // feed silence to the microphone input
-void Mic_FeedSilence();
+void Mic_FeedSilence(NDS& nds);
 
 // feed random noise to the microphone input
-void Mic_FeedNoise();
+void Mic_FeedNoise(NDS& nds);
 
 // feed an external buffer to the microphone input
 // buffer should be mono
-void Mic_FeedExternalBuffer();
+void Mic_FeedExternalBuffer(NDS& nds);
 void Mic_SetExternalBuffer(s16* buffer, u32 len);
 
 }
