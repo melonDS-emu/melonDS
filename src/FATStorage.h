f0aed8e49133b9c630df28af2af90ba5a40b97e2
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-11-21 20:44:51 -0500

    Add FATStorage::IsReadOnly

--- src/FATStorage.h
+++ src/FATStorage.h
@@ -47,6 +47,7 @@ public:
 
     u32 ReadSectors(u32 start, u32 num, u8* data);
     u32 WriteSectors(u32 start, u32 num, u8* data);
+    [[nodiscard]] bool IsReadOnly() const noexcept { return ReadOnly; }
 
 private:
     std::string FilePath;
