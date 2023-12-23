554f98d6a042a2537bec31dcbcf88faa1e463543
Raining Chain <rainingchain@gmail.com>
2023-12-23 14:46:06 -0500

    Support GBA .sav file with appended .rtc

--- src/GBACart.cpp
+++ src/GBACart.cpp
@@ -193,6 +193,7 @@ void CartGame::SetupSave(u32 type)
         SRAMType = S_FLASH512K;
         break;
     case 128*1024:
+    case (128*1024 + 0x10): // mGBA .sav file with appended real time clock data
         SRAMType = S_FLASH1M;
         break;
     case 0:
