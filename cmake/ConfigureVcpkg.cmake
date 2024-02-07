dd2a87f938f24a3116430b2d13a0802f1cfd1e53
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-02-07 20:54:36 +0100

    vcpkg 2024.01.12

--- cmake/ConfigureVcpkg.cmake
+++ cmake/ConfigureVcpkg.cmake
@@ -7,7 +7,7 @@ if (VCPKG_ROOT STREQUAL "${_DEFAULT_VCPKG_ROOT}")
     file(LOCK "${_DEFAULT_VCPKG_ROOT}" DIRECTORY GUARD FILE)
     FetchContent_Declare(vcpkg
         GIT_REPOSITORY "https://github.com/Microsoft/vcpkg.git"
-        GIT_TAG 2023.12.12
+        GIT_TAG 2024.01.12
         SOURCE_DIR "${CMAKE_SOURCE_DIR}/vcpkg")
     FetchContent_MakeAvailable(vcpkg)
 endif()
