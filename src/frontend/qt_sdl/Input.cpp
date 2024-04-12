8feeee61037b23fd0cb22d2f035f6ebfa0c76e32
Arisotura <thetotalworm@gmail.com>
2024-04-12 20:02:16 +0200

    Input: only check joystick input if a joystick actually exists

--- src/frontend/qt_sdl/Input.cpp
+++ src/frontend/qt_sdl/Input.cpp
@@ -209,16 +209,22 @@ void Process()
     }
 
     JoyInputMask = 0xFFF;
-    for (int i = 0; i < 12; i++)
-        if (JoystickButtonDown(Config::JoyMapping[i]))
-            JoyInputMask &= ~(1<<i);
+    if (Joystick)
+    {
+        for (int i = 0; i < 12; i++)
+            if (JoystickButtonDown(Config::JoyMapping[i]))
+                JoyInputMask &= ~(1 << i);
+    }
 
     InputMask = KeyInputMask & JoyInputMask;
 
     JoyHotkeyMask = 0;
-    for (int i = 0; i < HK_MAX; i++)
-        if (JoystickButtonDown(Config::HKJoyMapping[i]))
-            JoyHotkeyMask |= (1<<i);
+    if (Joystick)
+    {
+        for (int i = 0; i < HK_MAX; i++)
+            if (JoystickButtonDown(Config::HKJoyMapping[i]))
+                JoyHotkeyMask |= (1 << i);
+    }
 
     HotkeyMask = KeyHotkeyMask | JoyHotkeyMask;
     HotkeyPress = HotkeyMask & ~LastHotkeyMask;
