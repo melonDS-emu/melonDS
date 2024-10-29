51110524cacb0561b33a8b25eecaf11c39c9ae75
Jesse Talavera <jesse@jesse.tg>
2024-10-29 11:56:46 -0400

    Allow `CartGameSolarSensor::LightLevel` to be set explicitly

--- src/GBACart.h
+++ src/GBACart.h
@@ -158,6 +158,7 @@ public:
     void DoSavestate(Savestate* file) override;
 
     int SetInput(int num, bool pressed) override;
+    void SetLightLevel(u8 level) noexcept;
 
 protected:
     void ProcessGPIO() override;
