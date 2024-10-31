6c6cefad6c4add22cd01ca665301219fd6b2c0b0
Arisotura <thetotalworm@gmail.com>
2024-10-31 19:26:41 +0100

    add splashscreen

--- src/frontend/qt_sdl/Screen.h
+++ src/frontend/qt_sdl/Screen.h
@@ -110,12 +110,19 @@ protected:
 
         bool rendered;
         QImage bitmap;
+
+        int rainbowstart;
+        int rainbowend;
     };
 
     QMutex osdMutex;
     bool osdEnabled;
     unsigned int osdID;
     std::deque<OSDItem> osdItems;
+
+    QPixmap splashLogo;
+    OSDItem splashText[3];
+    QPoint splashPos[4];
 
     void loadConfig();
 
@@ -141,6 +148,8 @@ protected:
     virtual void osdDeleteItem(OSDItem* item);
 
     void osdUpdate();
+
+    void calcSplashLayout();
 };
 
 
@@ -202,7 +211,7 @@ private:
     GLuint screenVertexBuffer, screenVertexArray;
     GLuint screenTexture;
     GLuint screenShaderProgram;
-    GLuint screenShaderTransformULoc, screenShaderScreenSizeULoc;
+    GLint screenShaderTransformULoc, screenShaderScreenSizeULoc;
 
     QMutex screenSettingsLock;
     WindowInfo windowInfo;
@@ -211,10 +220,13 @@ private:
 
     GLuint osdShader;
     GLint osdScreenSizeULoc, osdPosULoc, osdSizeULoc;
-    GLfloat osdScaleFactorULoc;
+    GLint osdScaleFactorULoc;
+    GLint osdTexScaleULoc;
     GLuint osdVertexArray;
     GLuint osdVertexBuffer;
     std::map<unsigned int, GLuint> osdTextures;
+
+    GLuint logoTexture;
 
     void osdRenderItem(OSDItem* item) override;
     void osdDeleteItem(OSDItem* item) override;
