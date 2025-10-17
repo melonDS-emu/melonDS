2c919740ca5d11ccc015bc3368e8e82eb33b0ba2
Arisotura <thetotalworm@gmail.com>
2025-10-17 23:38:02 +0200

    add comment to describe usrcheat.dat format

--- src/ARDatabaseDAT.cpp
+++ src/ARDatabaseDAT.cpp
@@ -61,6 +61,51 @@ bool ARDatabaseDAT::Load()
     if (!f) return true;
 
     // todo: load
+    //
+    /* FILE FORMAT
+
+     header:
+     00: ID string "R4 CheatCode" (12 bytes)
+     0C: offset to entry list? (always 0x100)
+     10: database description
+     50: database active flag
+     100: entry list
+
+     entry:
+     00: game code
+     04: checksum of ROM header (logical NOT of CRC32 over first 512 bytes)
+     08: offset to cheat data (absolute)
+     0C: zero
+
+     the entry list is terminated with an all-zero entry
+
+     cheat data: header then items
+
+     header:
+     * game name (null-term string)
+     * padding (to align entry below to 4-byte boundary)
+     * flags (word): bit0-27 = total number of items, bit28-31 = game active (0=inactive, F=active)
+     * master codes (8 words) - use unclear
+
+     item:
+     * flags (word): bit0-23 = data length in words, bit24 = flag, bit28 = type
+     * item name (null-term string)
+     * item description (null-term string)
+     * padding (to align data below to 4-byte boundary)
+     * item data
+
+     meaning of item type/flag:
+     * type=0: cheat, bit24 = cheat active
+     * type=1: category, bit24 = only one cheat may be active in this category
+
+     empty description still takes up one extra byte (for the null terminator)
+
+     for a category: item data is a list of cheat items
+
+     for a cheat: item data is as follows:
+     * number of code words (word)
+     * code
+    */
 
     CloseFile(f);
     return true;
