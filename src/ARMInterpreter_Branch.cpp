241b2b8877b50b9b92111b27a534b1116eb7fab4
Jakly <102590697+Jaklyy@users.noreply.github.com>
2025-11-16 03:04:50 -0500

    fix minor thumb blx immediate bugs (#2450)

--- src/ARMInterpreter_Branch.cpp
+++ src/ARMInterpreter_Branch.cpp
@@ -106,11 +106,22 @@ void T_BL_LONG_2(ARM* cpu)
 {
     s32 offset = (cpu->CurInstr & 0x7FF) << 1;
     u32 pc = cpu->R[14] + offset;
-    cpu->R[14] = (cpu->R[15] - 2) | 1;
 
-    if ((cpu->Num==1) || (cpu->CurInstr & (1<<12)))
+    if ((cpu->Num==1) || (cpu->CurInstr & (1<<12))) // BL
+    {
         pc |= 1;
+    }
+    else // BLX
+    {
+        if (cpu->CurInstr & 1) // lsb of immediate is set, implying halfword offset; this raises undefined.
+            return T_UNK(cpu);
 
+        // instruction always switches to arm mode
+        // interworking bit should be cleared.
+        pc &= ~1;
+    }
+
+    cpu->R[14] = (cpu->R[15] - 2) | 1;
     cpu->JumpTo(pc);
 }
 
