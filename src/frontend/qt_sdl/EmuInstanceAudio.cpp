968bacf4c5ae32427284eb99c58f2e9f99afa3b1
Nadia Holmquist Pedersen <nadia@nhp.sh>
2025-11-17 16:20:38 +0100

    reduce the hardcoded frontend audio buffer size to 512
    seems to fix crackling on Windows

--- src/frontend/qt_sdl/EmuInstanceAudio.cpp
+++ src/frontend/qt_sdl/EmuInstanceAudio.cpp
@@ -41,7 +41,7 @@ void EmuInstance::audioInit()
     audioSyncLock = SDL_CreateMutex();
 
     audioFreq = 48000; // TODO: make both of these configurable?
-    audioBufSize = 1024;
+    audioBufSize = 512;
 
     SDL_AudioSpec whatIwant, whatIget;
     memset(&whatIwant, 0, sizeof(SDL_AudioSpec));
