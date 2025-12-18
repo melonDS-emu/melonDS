fcf6a8fa26f6142ee39888f52ca516949cd89586
PanMenel <90268767+PanMenel@users.noreply.github.com>
2025-12-18 22:39:08 +0100

    Gówno fix usuwający feature

--- src/frontend/qt_sdl/EmuInstanceInput.cpp
+++ src/frontend/qt_sdl/EmuInstanceInput.cpp
@@ -67,7 +67,6 @@ const char* EmuInstance::hotkeyNames[HK_MAX] =
     "HK_GuitarGripRed",
     "HK_GuitarGripYellow",
     "HK_GuitarGripBlue",
-    "HK_MenuBarToggle"
 };
 
 std::shared_ptr<SDL_mutex> EmuInstance::joyMutexGlobal = nullptr;
