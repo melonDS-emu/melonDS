b3dd9880768a5d36c8daf87229b0bc915b51c1ff
Jakly <102590697+Jaklyy@users.noreply.github.com>
2026-07-29 15:18:30 -0400

    fix vcount writing causing hangs (#2710)
    * fix vcount writes hanging the sw renderer if the value written is the same as the previous value in the internal reg
    
    * that variable isn't doing much anymore

--- src/GPU.cpp
+++ src/GPU.cpp
@@ -1384,10 +1384,9 @@ void GPU::SetVCount(u16 val, u16 mask) noexcept
     // 3D engine seems to give up on the current frame in that situation, repeating the last two scanlines
     // TODO: also check the various DMA types that can be involved
 
-    u16 nextvc = (NextVCount & ~mask) | (val & mask);
+    NextVCount = (NextVCount & ~mask) | (val & mask);
 
-    GPU3D.AbortFrame |= NextVCount != nextvc;
-    NextVCount = nextvc;
+    GPU3D.AbortFrame = true; // CHECKME: this probably shouldn't be done if the vcount written is the same as the vcount of the next scanline
     VCountOverride = true;
 }
 
